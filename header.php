c8764d6c41ade96a67a8573acb57cd45e2e0c24d
giovannimalausa <83951798+giovannimalausa@users.noreply.github.com>
2023-06-13 15:36:10 +0200

    Esperimenti con PHP

---
+++ header.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Header by Giovanni Malausa
+ * 
+ * @package giovannimalausa
+ * @since gm 1.0.0
+ * @license GPL 2.0
+ * 
+ */
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+
+	<meta charset="<?php bloginfo('charset'); ?>" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	
+	<!-- Responsive stylesheet -->
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	
+	<?php indented_wp_head(); ?>
+
+</head>
+
+<body <?php body_class(); ?>>
